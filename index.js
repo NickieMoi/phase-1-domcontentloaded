ca2bc63c344219a21beffbcf265b77fea82cba6f
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-06-13 15:32:48 +0300

    commit tree 28dd3318a7b530423e72668f0a0d0f75b5f1548b

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+function domUpdate() {
+  document.addEventListener("DOMContentLoaded", function () {
+    modifyDom();
+  });
+}
+
+function modifyDom() {
+  document.getElementById("text").innerHTML = "This is really cool";
+}
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("text").innerHTML = "This is really cool!";
+});
